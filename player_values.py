a74768e0178d31154a4f6dff7150a596cd63eff6
Joshua Chudy <jchudy@isi.edu>
2017-03-24 00:58:13 -0700

    added fielding values

--- player_values.py
+++ player_values.py
@@ -19,8 +19,10 @@ with open('batting2016.csv', 'rb') as battingFile:
             continue
 
         key = row[1].split('\\')[0]
+        if key.find('*') > -1:
+            key = key.split('*')[0]
         battingStatsDict[key] = row
-
+    battingFile.closed
 
 # ['Rk', 'Name', 'Age', 'Tm', 'Lg', 'G', 'PA', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'SB', 'CS', 'BB', 'SO', 'BA', 'OBP', 'SLG', 'OPS', 'OPS+', 'TB', 'GDP', 'HBP', 'SH', 'SF', 'IBB', 'Pos Summary']
 # indices in battingStatsDict[x]:
@@ -53,7 +55,6 @@ battingTransform = {
     'GDP': [24, -0.5]
 }
 
-
 for key1 in battingStatsDict.iteritems():
     playerName = key1[0]
     # array of stat values
@@ -65,7 +66,51 @@ for key1 in battingStatsDict.iteritems():
         index = key2[1][0]
         modifier = key2[1][1]
         if stat == '1B':
-            value1B = float( float(statRow[9]) - (float(statRow[10]) + float(statRow[11]) + float(statRow[12])) )
+            value1B = float(statRow[9]) - (float(statRow[10]) + float(statRow[11]) + float(statRow[12]))
             battingValuesDict[playerName] += value1B
             continue
         battingValuesDict[playerName] += float(statRow[index]) * modifier
+
+    # print battingValuesDict[playerName]
+
+# dictionary of player stats
+# key = player name
+# value = player's stat row
+fieldingStatsDict = {}
+
+with open('fielding2016.csv', 'rb') as fieldingFile:
+    fieldingReader = csv.reader(fieldingFile)
+    i = 0
+    for row in fieldingReader:
+        if i == 0:
+            i += 1
+            continue
+
+        key = row[1].split('\\')[0]
+        fieldingStatsDict[key] = row
+    fieldingFile.closed
+
+# ['Rk', 'Name', 'Age', 'Tm', 'Lg', 'G', 'GS', 'CG', 'Inn', 'Ch', 'PO', 'A', 'E', 'DP', 'Fld%', 'Rtot', 'Rtot/yr', 'Rdrs', 'Rdrs/yr', 'RF/9', 'RF/G', 'Pos Summary']
+# indices in fieldingStatsDict[x]:
+# E = 12                               [-1]
+# OFA = if indexOf('OF') in 21 { 11 }  [1]
+
+for key1 in fieldingStatsDict.iteritems():
+    playerName = key1[0]
+    # array of stat values
+    statRow = key1[1]
+     # calculate errors value
+    fieldingValue = (float(statRow[12]) * -1)
+
+    #add OFA assists if OF in position summary
+    if statRow[21].find('OF'):
+        fieldingValue += float(statRow[11])
+
+
+    if not playerName in battingValuesDict:
+        battingValuesDict[playerName] = 0
+
+    battingValuesDict[playerName] += fieldingValue
+
+    if battingValuesDict[playerName] > 700:
+        print (playerName, ": ", battingValuesDict[playerName])
